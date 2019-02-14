57d9137c406755e9d8be67b0c567b43dd8cb1908
songmingming <1659672814@qq.com>
2019-02-14 11:52:28 +0800

    路由按需加载

--- src/router.js
+++ src/router.js
@@ -1,20 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import MapP from './components/Map.vue'
-import MaskMap from './components/MaskMap.vue'
+// import MapP from './components/Map.vue'
+// import MaskMap from './components/MaskMap.vue'
 import HelloWorld from './components/HelloWorld.vue'
-import T1 from './components/Test1.vue'
+// 异步按需加载
+const MapP = resolve => require(['./components/Map.vue'], resolve) 
+const MaskMap = resolve => require(['./components/MaskMap.vue'], resolve)
+const T1 = resolve => require(['./components/Test1.vue'], resolve)
 
 Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/map',
+      path: '/',
       name: 'map',
       component: MapP
     },
     {
-      path: '/',
+      path: '/T1',
       name: 'T1',
       component: T1
     },
